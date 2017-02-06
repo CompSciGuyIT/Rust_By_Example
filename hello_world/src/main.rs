8b0fca3618faf865f6dea0562a7eed3ac8718366
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-06 18:18:25 +1000

    Hello World - Formatted print - Display

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,52 @@
+// import the 'fmt' module to make it available
+use std::fmt;
+
+// Define a structure which 'fmt::Display' will be implemented for
+// A tuple struct containing an 'i32' bound to the name 'Structure'
+struct Structure(i32);
+
+// In order to use the '{}' marker, the trait 'fmt::Display'
+// must be implemented manually for this type
+impl fmt::Display for Structure {
+    // This trait requires 'fmt' with this exact signature
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output stream: 'f'.
+        // Returns 'fmt::Result' which indicates whether operation succeeded or failed.
+        // Note that 'write!' uses syntax which is very similar to 'println!'
+        write!(f, "{}", self.0)  
+    }
+}
+
+// A structure holding two numbers
+// 'Debug' will be derived so the results can be contrasted with 'Display'
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+// Implement 'Display' for 'MinMax'
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use 'self.number' to refer to each positional data point
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+// Define a structure where the fields are namable for comparison
+#[derive(Debug)]
+struct Point2 {
+    x: f64,
+    y: f64,
+}
+
+// Implement 'Display' for 'Point2'
+impl fmt::Display for Point2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customise so only 'x' and 'y' are denoted
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
 fn main() {
+
     // Line comments which go to the end of the line
 
     /*
@@ -72,5 +120,30 @@ fn main() {
 
     // The problem with derive is there is no control
     // over how the results will look.
-    println!("Now {:?} will print!", Deep(DebugPrintable(7)));
+    println!("Now {:?} will print!\n", Deep(DebugPrintable(7)));
+
+    // The code for the Display section
+    let minmax = MinMax(0, 14);
+
+    println!("Compare Structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range =     MinMax(-300, 300);
+    let small_range =   MinMax(-3, 3);
+
+    println!("\nThe big range is {big} and the small range is {small}\n",
+            small = small_range,
+            big = big_range);
+    
+    let point = Point2 { x: 3.3, y: 7.2};
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // Error.  Both 'Debug' and 'Display' were implemented,
+    // but '{:b}' requires 'fmt::Binary' to be implemented.
+    // This will not work.
+    // println!("What does Point2D look like in binary: {:b}?", point);
 }
