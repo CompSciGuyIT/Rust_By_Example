fa922cb8ffb1ddfc7403d460c560216ca26ef658
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-06 13:54:04 +1000

    Hello World - Comments

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
+    // Line comments which go to the end of the line
+
+    /*
+        Block comments which go to the
+        closing delimiter
+    */
+
+    /*
+        /// Generate library docs for the following item
+        //! Generate library docs for the enclosing item
+    */
+    
     println!("Hello, world!");
     println!("I'm a Rustacean!");
 }
