162d4eb80a3e394b18c87c16147dec154cc13142
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-07 15:25:11 +1000

    Hello World - Formatted print - Testcase: List

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -59,6 +59,20 @@ impl fmt::Display for Complex {
     }
 }
 
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let List(ref vec) = *self;
+        try!(write!(f, "["));
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { try!(write!(f, ", "));}
+            try!(write!(f, "{}: {}", count, v));
+        }
+        write!(f, "]")
+    }
+}
+
 fn main() {
 
     // Line comments which go to the end of the line
@@ -171,4 +185,9 @@ fn main() {
     
     println!("Display: {}", complex);
     println!("Debug: {:?}", complex);
+
+    println!("\n");
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
