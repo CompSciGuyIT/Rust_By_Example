bc47fe017799151b56f8fba7ec14b578a76fb680
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-13 18:31:35 +1000

    Custom Types - Constants

--- custom_types/src/main.rs
+++ custom_types/src/main.rs
@@ -3,6 +3,15 @@
 
 use List::*;
 
+// Globals are declared outside all other scopes
+static LANGUAGE: &'static str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    // Access constant in some function
+    n > THRESHOLD
+}
+
 // Create an enum to classify someone.
 // Note how both names and type information
 // together specify the variant:
@@ -264,4 +273,20 @@ fn main() {
     // Show the final state of the list
     println!("linked list gas length: {}", list.len());
     println!("{}", list.stringify());
+
+    //////////////////////////////
+    // Constants
+    //////////////////////////////
+
+    println!();
+    
+    let n = 16;
+
+    // Access constant in the main thread
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // Error! Cannot modify a constant
+    // THRESHOLD = 5;
 }
