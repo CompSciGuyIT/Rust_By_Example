931c2f0b786b6cd794818df4d43a75cb234c60c8
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-15 16:45:12 +1000

    Variable Bindings - Declare first

--- variable_bindings/src/main.rs
+++ variable_bindings/src/main.rs
@@ -39,7 +39,7 @@ fn main() {
     //////////////////////////
 
     println!();
-    
+
     // This binding lives in the main function
     let long_lived_binding = 1;
 
@@ -65,4 +65,32 @@ fn main() {
     let long_lived_binding = 'a';
 
     println!("outer long: {}", long_lived_binding);
+
+    /////////////////////////
+    // Declare First
+    ////////////////////////
+
+    println!();
+    
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+
+        // initialise the binding
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // Error! Use of uninitialised binding
+    // println!("another binding: {}", another_binding);
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
+
 }
