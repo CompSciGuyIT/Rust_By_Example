3fe95895e333e7db08222b326266ff63351525a8
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-15 16:17:30 +1000

    Variable Bindings - Mutability

--- variable_bindings/src/main.rs
+++ variable_bindings/src/main.rs
@@ -15,5 +15,22 @@ fn main() {
     // the variable name with an underscore
     let _unused_variable = 3u32;
 
+    //////////////////////
+    // Mutability
+    /////////////////////
+
+    println!();
     
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error
+    // _immutable_binding += 1;
 }
